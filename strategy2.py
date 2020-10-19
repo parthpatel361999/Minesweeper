bee145b15fd3874efd4c6f02e9e50b8e624b0be9
Parth Patel <60615655+parthpatel361999@users.noreply.github.com>
2020-10-19 14:52:47 -0400

    Update strat-2 algorithm and comment

--- strategy2.py
+++ strategy2.py
@@ -1,5 +1,5 @@
 from Agent import Agent,Cell 
-from Board import Board
+from Board import Board, findNeighboringCoords
 from queue import Queue
 import random as rnd
 import numpy as np
@@ -7,26 +7,36 @@ import numpy as np
 def strategy2(gboard, dim, agent):
     coeffs = []
     constants = []
-    inferredSafeQueue = Queue(maxsize = dim*dim)
     cellTypes = []
+    inferredSafeQueue = Queue(maxsize = dim*dim)
     
     r = rnd.randint(0,dim-1)
     c = rnd.randint(0,dim-1)
-    while((dim*dim) - len(agent.revealedCoords) - len(agent.identifiedMineCoords) != 0):
-        while ((r,c) in agent.revealedCoords or (r,c) in agent.identifiedMineCoords):
+    while((dim*dim) - len(agent.revealedCoords) - len(agent.identifiedMineCoords) != 0): #run until all cells are explored
+        while ((r,c) in agent.revealedCoords or (r,c) in agent.identifiedMineCoords): #pick coordinates again if cell has already been explored
             r = rnd.randint(0,dim-1)
             c = rnd.randint(0,dim-1)
-        currentCell = agent.checkCell((r,c),gboard)
+        currentCell = agent.checkCell((r,c),gboard) #check the cell at (r, c)
         if (currentCell.type == -1):
             newEq = np.zeros([dim**2], dtype=int)
             newEq[tupleToIndex(r, c, dim)] = 1
-            coeffs.append(newEq)
-            constants.extend([1])
-            cellTypes = np.linalg.solve(coeffs, constants)
-
+            coeffs.append(newEq) #add the new equation to the KB matrix
+            constants.extend([1]) #add 1 to the constants vector
+            cellTypes = np.linalg.solve(coeffs, constants) #attempt to solve the new system
+            #check to see if new cells are inferred safe now
+            #add those inferred safe cells to inferredSafeQeue and choose next cell from there
+            #if the inferredSafeQueue is empty generate a random cell to inspect
         else:
-            cellTypes[currentCell.coords[0]][currentCell.coords[1]] = 0
-            cellClues[currentCell.coords[0]][currentCell.coords[1]] = currentCell.type
+            newEq = np.zeros([dim**2], dtype=int)
+            neighbors = findNeighboringCoords((r, c), dim)
+            for n in neighbors:
+                newEq[tupleToIndex(n[0], n[1], dim)] = 1
+            coeffs.append(newEq) #add the new equation to the KB matrix
+            constants.extend(currentCell.type) #add the clue value to the constants vector
+            cellTypes = np.linalg.solve(coeffs, constants) #attempt to solve the new system
+            #check to see if new cells are inferred safe now
+            #add those inferred safe cells to inferredSafeQeue and choose next cell from there
+            #if the inferredSafeQueue is empty generate a random cell to inspect
 
 
         print((r,c))
@@ -36,7 +46,6 @@ def tupleToIndex(r, c, dim):
     return dim * r + c #converts cell tuples to unique integers
 def indexToTuple(i, dim):
     return (int(i / dim), i % dim) #converts the unique integers back into their respective tuples
-
 def display(dim,agent):
     display = Board(dim)
     for i in range(0,dim):
