73a49fdd8d2733859e1326f52e58cbac95dcc849
Rumeet Goradia <rumeet.goradia@gmail.com>
2020-10-22 17:29:38 -0400

    Finish game loop and add preliminary comments

--- strategy3.py
+++ strategy3.py
@@ -6,44 +6,133 @@ from Agent import Agent, Cell
 from Board import Board
 from strategy2 import addEq, indexToTuple, tupleToIndex
 
-"""
+
+# double improved agent
+def strategy3(gboard, dim, agent):
+    """
+    Declare a list for the knowledge base, which will be filled with equations (represented as lists 
+    themselves). Also declare a set for the unknown variables in the knowledge base.
+    """
     KB = []
-    varList = []
-    simulatedBombVars = []
-    bombCounts = {}
-    -----------
-    validConfigs = 0
-    -----------
-    1. add first var in varList to KB as safe (var = 0)
-    2. if leaf node:
-        a. if valid config:
-            i. validConfigs += 1
-            ii. for var in simulatedBombVars: bombCounts[var] += 1
-    3. else:
-        a. if valid config:
-            validConfigts += function(KB, varList[1, end], simulatedBombVars, bombVars)
-    4. add first var in varList to KB as bomb (var = 1)
-    5. add var to simulatedBombVars
-    6. if leaf node:
-        a. if valid config:
-            i. validConfigs += 1
-            ii. for var in simulatedBombVars: bombCounts[var] += 1
-    7. else:
-        a. if valid config:
-            validConfigs += function(KB, varList[1, end], simulatedBombVars, bombVars)
-    8. return validConfigs        
+    variables = set()
 
-"""
+    """
+    Create a list of the preferred coordinates for the agent to choose from when no better option is
+    available. In thise case, the preferred coordinates are the corners of the minefield. For the
+    first choice of where the agent should explore, choose the first (top-left) corner.
+    """
+    corners = [(0, 0), (0, dim - 1), (dim - 1, 0),
+               (dim - 1, dim - 1)]
+    cornerIndex = 0
+    r, c = corners[cornerIndex]
+
+    while not agent.isFinished():
+        """
+        Check the current cell. Based on the type of the mine, add the respective equations and variables.
+        """
+        currentCell = agent.checkCell((r, c), gboard)
+        if (currentCell.type == Cell.MINE):
+            KB = addMineEq(KB, currentCell, dim)
+        else:
+            KB = addSafeEq(KB, currentCell, dim, agent, variables)
+
+        """
+        If there are no unknown variables in the knowledge base, choose one of the preferred coordinates.
+        If all preferred coordinates have been explored, choose random coordinates.
+
+        There is no point in calculating probabilities if there are no variables for which to calculate
+        probabilities, so choose the preferred coordinates instead.
+        """
+        if len(variables) == 0:
+            cornerIndex += 1
+            if cornerIndex < len(corners):
+                r, c = corners[cornerIndex]
+            else:
+                r, c = agent.chooseRandomCoords()
+
+        else:
+            """
+            Calculate the probabilities for all variables, and find the variables that are guaranteed to
+            be safe and those that are guaranteed to be mines. For such variables, remove them from the
+            unknown variables set and check or identify the corresponding coordinates. Add the respective
+            equations and variables. Then, if there is a cell that is least likely to be a mine, set its
+            coordinates as the next to be explored; else, choose from the preferred coordinates (or
+            random coordinates, if necessary).
+            """
+            safeVariables, mineVariables, safestVariable = calculateVariableProbabilities(
+                KB, list(variables))
+            for variable in safeVariables:
+                variables.remove(variable)
+                safeCell = agent.checkCell(indexToTuple(variable, dim), gboard)
+                KB = addSafeEq(KB, safeCell, dim, variables)
+            for variable in mineVariables:
+                variables.remove(variable)
+                mineCell = agent.identifyMine(indexToTuple(variable, dim))
+                KB = addMineEq(KB, mineCell, dim)
+            if not safestVariable is None:
+                r, c = indexToTuple(safestVariable, dim)
+            else:
+                cornerIndex += 1
+                if cornerIndex < len(corners):
+                    r, c = corners[cornerIndex]
+                else:
+                    r, c = agent.chooseRandomCoords()
 
 
-def configIsValid(KB):
-    for eq in KB:
-        if len(eq) - 1 > eq[-1] or eq[-1] < 0:
-            return False
-    return True
+def addSafeEq(KB, cell, dim, agent, variables=set()):
+    """
+    Add an equation identifying the cell's coordinates as safe to the knowledge base. Then, create a clue 
+    equation for all of the cell's neighbors, and add those neighbors to the unknown variables list if
+    they haven't been explored by the agent already.
+    """
+    r, c = cell.coords
+    safeEq = [tupleToIndex(r, c, dim), 0]
+    KB = addEq(KB, safeEq)
+    clueEq = []
+    for neighbor in cell.neighbors:
+        nRow, nCol = neighbor
+        neighborIndex = tupleToIndex(nRow, nCol, dim)
+        clueEq.append(neighborIndex)
+        if not agent.hasExplored((nRow, nCol)):
+            variables.add(neighborIndex)
+    clueEq.append(cell.type)
+    KB = addEq(KB, clueEq)
+    return KB
 
 
-def findValidConfigs(KB, variables, simulatedBombVariables, bombCounts):
+def addMineEq(KB, cell, dim):
+    """
+    Add an equation identifying the cell's coordinates as a mine to the knowledge base. 
+    """
+    r, c = cell.coords
+    mineEq = [tupleToIndex(r, c, dim), 1]
+    KB = addEq(KB, mineEq)
+    return KB
+
+
+def calculateVariableProbabilities(KB, variables):
+    mineCounts = {}
+    for variable in variables:
+        mineCounts[variable] = 0
+    validConfigurations = findValidConfigs(KB, variables, [], mineCounts)
+    safeVariables = []
+    mineVariables = []
+    minMineProb = 1
+    safestVariable = None
+    for variable in mineCounts:
+        if mineCounts[variable] == 0:
+            safeVariables.append(variable)
+        elif mineCounts[variable] == validConfigurations:
+            mineVariables.append(variable)
+        else:
+            mineProb = mineCounts[variable] / validConfigurations
+            if mineProb < minMineProb:
+                minMineProb = mineProb
+                safestVariable = variable
+    return safeVariables, mineVariables, safestVariable
+
+
+def findValidConfigs(KB, variables, simulatedMineVariables, mineCounts):
     validConfigs = 0
     variable = variables[0]
 
@@ -53,82 +142,30 @@ def findValidConfigs(KB, variables, simulatedBombVariables, bombCounts):
     safeConfigIsValid = configIsValid(safeKB)
     if len(variables) == 1 and safeConfigIsValid:
         validConfigs += 1
-        for bombVar in simulatedBombVariables:
-            bombCounts[bombVar] += 1
+        for mineVar in simulatedMineVariables:
+            mineCounts[mineVar] += 1
     elif safeConfigIsValid:
         validConfigs += findValidConfigs(
-            safeKB, variables[1:], simulatedBombVariables.copy(), bombCounts)
+            safeKB, variables[1:], simulatedMineVariables.copy(), mineCounts)
 
-    bombEq = [variable, 1]
-    bombKB = copy.deepcopy(KB)
-    bombKB = addEq(bombKB, bombEq)
-    simulatedBombVariables.append(variable)
-    bombConfigIsValid = configIsValid(bombKB)
-    if len(variables) == 1 and bombConfigIsValid:
+    mineEq = [variable, 1]
+    mineKB = copy.deepcopy(KB)
+    mineKB = addEq(mineKB, mineEq)
+    simulatedMineVariables.append(variable)
+    mineConfigIsValid = configIsValid(mineKB)
+    if len(variables) == 1 and mineConfigIsValid:
         validConfigs += 1
-        for bombVar in simulatedBombVariables:
-            bombCounts[bombVar] += 1
-    elif bombConfigIsValid:
+        for mineVar in simulatedMineVariables:
+            mineCounts[mineVar] += 1
+    elif mineConfigIsValid:
         validConfigs += findValidConfigs(
-            bombKB, variables[1:], simulatedBombVariables.copy(), bombCounts)
+            mineKB, variables[1:], simulatedMineVariables.copy(), mineCounts)
 
     return validConfigs
 
 
-# double improved agent
-def strategy3(gboard, dim, agent):
-    """
-    Basic Outline for Algo:
-        1. Pick one of the corner cells
-        2. Compute probability that a cell contains a mine, for all cells
-        3. For cell in board, if P(cell = mine) = 1, mark cell as mine
-        4. For cell in board, if P(cell = mine) = 0, reveal and add to knowledge base
-        5. If no cell was safe, pick minimum probability in the board
-    """
-
-    KB = []
-    variables = []
-    inferredSafeCoords = deque()
-    inferredMineCoords = deque()
-
-    corners = [(0, 0), (0, dim - 1), (dim - 1, 0),
-               (dim - 1, dim - 1)]
-    cornerIndex = 0
-    r, c = corners[cornerIndex]
-
-    while not agent.isFinished():
-        currentCell = agent.checkCell((r, c), gboard)
-        if (currentCell.type == Cell.MINE):
-            bombEq = [tupleToIndex(r, c, dim), 1]
-            KB = addEq(KB, bombEq)
-        else:
-            safeEq = [tupleToIndex(r, c, dim), 0]
-            KB = addEq(KB, safeEq)
-            clueEq = []
-            # find all neighbors of (r, c)
-            for neighbor in currentCell.neighbors:
-                nrow, ncol = neighbor
-                neighborIndex = tupleToIndex(nrow, ncol, dim)
-                clueEq.append(neighborIndex)
-                variables.append(neighborIndex)
-            clueEq.append(currentCell.type)
-            KB = addEq(KB, clueEq)
-        if len(variables) == 0:
-            cornerIndex += 1
-            if cornerIndex < len(corners):
-                r, c = corners[cornerIndex]
-            else:
-                r = rnd.randint(0, dim - 1)
-                c = rnd.randint(0, dim - 1)
-                while agent.hasExplored((r, c)):
-                    r = rnd.randint(0, dim - 1)
-                    c = rnd.randint(0, dim - 1)
-        else:
-            safeCoords, mineCoords, safestCoords = calculateCellProbabilities(
-                KB, variables)
-
-    return
-
-
-def calculateCellProbabilities(KB, variables):
-    return set(), set(), (0, 0)
+def configIsValid(KB):
+    for eq in KB:
+        if len(eq) - 1 > eq[-1] or eq[-1] < 0:
+            return False
+    return True
